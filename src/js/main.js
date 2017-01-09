d7b4d92107eaebe2cdd63b25919a1274c01668b0
Preston Richey <prichey@barkleyus.com>
2017-01-09 12:53:37 -0600

    Add libraries to repo; Init dollar shape recognizer

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,31 @@
+var PALETTE = ["#20171C", "#1E2A43", "#28377D", "#352747", "#F285A5", "#CA2E26", "#B84526", "#DA6C26", "#453121", "#916A47", "#EEB641", "#F6EB16", "#7F7D31", "#6EAD79", "#2A4621", "#F4EAE0"];
+var CURRENT_COLOR = PALETTE[0];
+var MOVES = []; // store global moves list
+var _R = new DollarRecognizer();
+
+// maybe?
+// moves = [
+//   {
+//     'type': 'colorChange',
+//     'old': '#20171C',
+//     'new': '#F285A5'
+//   },
+//   {
+//     'type': 'newPath',
+//     'ref': '???' // uuid? dom reference?
+//   },
+//   {
+//     'type': 'pathTransform',
+//     'ref': '???', // uuid? dom reference?
+//     'old': 'rotate(90deg)scale(1.5)', // ???
+//     'new': 'rotate(120deg)scale(-0.5)' // ???
+//   },
+//   // others?
+// ]
+
 function initControlPanel() {
   initColorPalette();
+  initCanvasDraw();
   initNew();
   initUndo();
   initPlay();
@@ -8,25 +34,14 @@ function initControlPanel() {
 }
 
 function initColorPalette() {
-  var palette = ["#20171C", "#1E2A43", "#28377D", "#352747", "#F285A5", "#CA2E26", "#B84526", "#DA6C26", "#453121", "#916A47", "#EEB641", "#F6EB16", "#7F7D31", "#6EAD79", "#2A4621", "#F4EAE0"];
-
-  // var paletteColors = d3.select('ul.palette-colors')
-  //       .data(palette)
-  //       .enter()
-  //       .append('li').append('svg').append('rect')
-  //       .attr('width', 50)
-  //       .attr('height', 50)
-  //       .attr('fill', function(d) { return d; });
-
   var paletteWrap = d3.select('ul.palette-colors');
   var paletteColorSize = 20;
   var paletteSelectedColorSize = 30;
   var paletteSelectedClass = 'palette-selected';
 
-  var CURRENT_COLOR = palette[0];
-
-  palette.forEach(function(d, i) {
+  PALETTE.forEach(function(d, i) {
     if (i == 0) {
+      // if first child, show as selected
       paletteWrap.append('li').append('svg')
         .attr('class', 'palette-color ' + paletteSelectedClass)
         .attr('width', paletteSelectedColorSize)
@@ -49,7 +64,8 @@ function initColorPalette() {
     }
   });
 
-  var paletteColors = d3.selectAll('.palette-colors li')
+  // hook up click
+  d3.selectAll('.palette-colors li')
       .on('click', function() {
         var $this = d3.select(this).select('svg.palette-color');
 
@@ -72,33 +88,9 @@ function initColorPalette() {
           CURRENT_COLOR = $this.select('rect').attr('fill');
         }
       });
+}
 
-  // var svg = d3.select('svg.main')
-  //   .append('rect')
-  //   .attr('id', 'foo')
-  //   .attr('class', 'selected')
-  //   .attr('width', 50)
-  //   .attr('height', 50);
-  //
-  // var selectedPaletteClass = 'selected';
-  //
-  // var foo = d3.select('rect#foo')
-  //   .on('click', function() {
-  //     var $this = d3.select(this);
-  //     if ($this.classed(selectedPaletteClass)) {
-  //       $this.attr('rx', 20)
-  //         .attr('ry', 20)
-  //         .attr('width', 40)
-  //         .attr('height', 40);
-  //     } else {
-  //       $this.attr('rx', 0)
-  //         .attr('ry', 0)
-  //         .attr('width', 50)
-  //         .attr('height', 50);
-  //     }
-  //     $this.classed(selectedPaletteClass, !$this.classed(selectedPaletteClass));
-  //   });
-
+function initCanvasDraw() {
   var line = d3.line()
     .curve(d3.curveBasis);
 
@@ -143,10 +135,13 @@ function initColorPalette() {
 
 function newPressed() {
   console.log('new pressed');
+  d3.selectAll('svg.main path').remove();
+  moves = [];
 }
 
 function undoPressed() {
   console.log('undo pressed');
+  d3.selectAll('svg.main path:last-child').remove();
 }
 
 function playPressed() {
@@ -183,55 +178,3 @@ function main() {
 }
 
 main();
-
-// var white = d3.rgb("white"),
-//     black = d3.rgb("black"),
-//     height = d3.select("canvas#picker").property("height");
-//
-// var channels = {
-//   h: {scale: d3.scale.linear().domain([0, 360]).range([0, height]), y: height / 2},
-//   s: {scale: d3.scale.linear().domain([1, 1]).range([height, height]), y: height / 2},
-//   l: {scale: d3.scale.linear().domain([1, 1]).range([height, height]), y: height / 2}
-// };
-//
-// var channel = d3.selectAll(".channel")
-//     .data(d3.entries(channels));
-//
-// var pickerCanvas = channel.select("canvas#picker")
-//     .call(d3.behavior.drag().on("drag", pickerDragged))
-//     .each(pickerRender);
-//
-// function pickerDragged(d) {
-//   d.value.x = Math.max(0, Math.min(this.height - 1, d3.mouse(this)[0]));
-//   pickerCanvas.each(pickerRender);
-// }
-//
-// function pickerRender(d) {
-//   var height = this.height,
-//       context = this.getContext("2d"),
-//       image = context.createImageData(height, 1),
-//       i = -1;
-//
-//   var current = d3.hsl(
-//     channels.h.scale.invert(channels.h.x),
-//     channels.s.scale.invert(channels.s.x),
-//     channels.l.scale.invert(channels.l.x)
-//   );
-//
-//   for (var y = 0, v, c; y < height; ++y) {
-//     if (y === d.value.y) {
-//       c = white;
-//     } else if (y === d.value.y - 1) {
-//       c = black;
-//     } else {
-//       current[d.key] = d.value.scale.invert(y);
-//       c = d3.rgb(current);
-//     }
-//     image.data[++i] = c.r;
-//     image.data[++i] = c.g;
-//     image.data[++i] = c.b;
-//     image.data[++i] = 255;
-//   }
-//
-//   context.putImageData(image, 0, 0);
-// }
