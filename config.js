499faca10909517a80116c8a5d9aa9fa6c2774b4
Preston Richey <prichey@barkleyus.com>
2017-01-17 18:03:12 -0600

    Add color pops to palette

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 exports.palette = {
   colors: ["#20171C", "#1E2A43", "#28377D", "#352747", "#CA2E26", "#9A2A1F", "#DA6C26", "#453121", "#916A47", "#DAAD27", "#7F7D31","#2B5E2E"],
+  pops: ["#00ADEF", "#F285A5", "#7DC57F", "#F6EB16", "#F4EAE0"],
   colorSize: 20,
   selectedColorSize: 30
 }
