a7e558c7d859e3b3e55e83d24e6154088075e04d
CK Hicks <ck@40digits.com>
2017-02-14 13:03:08 -0600

    Adding demo video interactions to: /#video

--- src/js/video.js
+++ src/js/video.js
@@ -6,12 +6,29 @@ const hammerManager = touch.hammerManager;
 const $body = $('body');
 const tapEvent = 'click tap touch';
 
-function playVideo() {
-  $body.addClass('play-video');
+const $videoWrapper = $body.find('#video');
+const $video = $videoWrapper.find('video');
+
+function videoTriggers() {
+  let counter;
+  $body.on(tapEvent, e => {
+    // hiding the video if playing
+    if ( $body.hasClass('play-video') ) {
+      $body.toggleClass('play-video');
+      clearTimeout(counter);
+    } else {
+      clearTimeout(counter);
+      // wait for another interaction, else play the video
+      counter = setTimeout(() => {
+        $body.toggleClass('play-video');
+      }, 10000); // 10 seconds
+    }
+  });
 }
 
 export function init() {
   if ( window.location.hash.length > 0 && window.location.hash == '#video' ) {
-    playVideo();
+    $body.toggleClass('play-video');
   }
+  videoTriggers();
 }
