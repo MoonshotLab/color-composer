1f2320c715dd01e1ee051466293da6c4cae7c039
CK Hicks <ck@40digits.com>
2017-02-15 17:09:18 -0600

    overlays update

--- src/js/video.js
+++ src/js/video.js
@@ -18,7 +18,7 @@ function videoTriggers() {
       // wait for another interaction, else play the video
       counter = setTimeout(() => {
         $body.toggleClass('play-video');
-      }, 30 * 1000); // 30 seconds
+      }, 5 * 60 * 1000); // 5 minutes
     }
   });
 }
