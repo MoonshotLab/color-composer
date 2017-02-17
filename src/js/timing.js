a20af43d638e9f8f72c5ea807956ef35d86dd5d2
Preston Richey <prichey@barkleyus.com>
2017-02-16 21:00:19 -0600

    Update tips copy; Don't close and reset overlays on activity timeout

--- src/js/timing.js
+++ src/js/timing.js
@@ -22,7 +22,7 @@ export function init() {
 
 export function preventInactivityTimeout() {
   console.log(`prevent timeout: ${util.getTime()}`);
-  overlays.closeAndResetOverlays();
+  // overlays.closeAndResetOverlays();
 
   clearTimeout(window.kan.inactivityTimeout);
 
