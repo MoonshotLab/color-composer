8c88b95d5995dfcfc9cd55b4225e4ec73e208521
Preston Richey <prichey@barkleyus.com>
2017-03-03 15:55:38 -0600

    Add comment

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.get('/process', function(req, res) {
     '-y'
   ].join(' ');
 
+  // https://trac.ffmpeg.org/wiki/Concatenate
   const makeIntoTransportStreamCommand = [
     ffmpegPath,
     `-i ${mergedOutPath}`,
