dfe385d29048ff33e5a35de81342eb022097bda2
Preston Richey <prichey@barkleyus.com>
2017-03-04 13:21:14 -0600

    Promisify stopping recording

--- src/js/share.js
+++ src/js/share.js
@@ -1,5 +1,7 @@
 const config = require('./../../config');
 
+const Promise = require('bluebird');
+
 const ui = require('./ui');
 const sound = require('./sound');
 
@@ -7,6 +9,32 @@ const drawCanvas = document.getElementById(config.canvasId);
 const canvasRecorder = RecordRTC(drawCanvas, {
   type: 'canvas'
 });
+
+function asyncStopAudioRecordingAndExportBlob(recorder) {
+  return new Promise(function(resolve, reject) {
+    try {
+      recorder.stop();
+      recorder.exportWAV(function(blob) {
+        resolve(blob);
+      });
+    } catch(e) {
+      reject(e);
+    }
+  });
+}
+
+function asyncStopVideoRecordingAndExportBlob(recorder) {
+  return new Promise(function(resolve, reject) {
+    try {
+      recorder.stopRecording(function() {
+        let blob = recorder.getBlob();
+        resolve(blob);
+      });
+    } catch(e) {
+      reject(e);
+    }
+  });
+}
 
 export function record() {
   let audioRecorder = new Recorder(Howler.masterGain, {
@@ -17,22 +45,38 @@ export function record() {
   audioRecorder.record();
 
   sound.asyncPlayCompositionOnce().then(() => {
-    audioRecorder.stop();
-    audioRecorder.exportWAV(function(blob) {
-      download(blob, 'blob.wav');
-    });
-    canvasRecorder.stopRecording(function() {
-      var blob = canvasRecorder.getBlob();
-      download(blob, 'blob.webm');
-    });
+    return Promise.all([asyncStopAudioRecordingAndExportBlob(audioRecorder), asyncStopVideoRecordingAndExportBlob(canvasRecorder)])
+      .then(function(values) {
+        let [audioBlob, videoBlob] = values;
+        download(audioBlob, 'blob.wav');
+        download(videoBlob, 'blob.webm');
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+    // audioRecorder.stop();
+    // audioRecorder.exportWAV(function(blob) {
+    //   download(blob, 'blob.wav');
+    // });
+    // canvasRecorder.stopRecording(function() {
+    //   var blob = canvasRecorder.getBlob();
+    //   download(blob, 'blob.webm');
+    // });
   }).error((e) => {
-    audioRecorder.stop();
-    audioRecorder.exportWAV(function(blob) {
-      download(blob, 'blob.wav');
-    });
-    canvasRecorder.stopRecording(function() {
-      var blob = canvasRecorder.getBlob();
-      download(blob, 'blob.webm');
-    });
+    Promise.all([asyncStopAudioRecordingAndExportBlob(audioRecorder), asyncStopVideoRecordingAndExportBlob(canvasRecorder)])
+      .then(function(values) {
+        console.log(values)
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+    // audioRecorder.stop();
+    // audioRecorder.exportWAV(function(blob) {
+    //   download(blob, 'blob.wav');
+    // });
+    // canvasRecorder.stopRecording(function() {
+    //   var blob = canvasRecorder.getBlob();
+    //   download(blob, 'blob.webm');
+    // });
   });
 }
