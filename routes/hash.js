79369f0d0b4bd276a70e221a21b86535c658aa2c
Preston Richey <prichey@barkleyus.com>
2017-03-04 22:11:12 -0600

    Attempt to fix hash function

--- routes/hash.js
+++ routes/hash.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const git = require('git-rev');
 
 router.get('/', function(req, res) {
+  console.log('process.env.GIT_REV', process.env.GIT_REV);
+  git.long(function(str) {
+    console.log('git.long', str);
+  });
+  
   if (!!process.env.GIT_REV) {
     res.send(process.env.GIT_REV);
   } else {
