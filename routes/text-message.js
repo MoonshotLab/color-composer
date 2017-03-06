6e5604b930e0f9fbf053253dd784b87d6ebb3261
Preston Richey <prichey@barkleyus.com>
2017-03-06 16:46:53 -0600

    Add debug for Twilio api

--- routes/text-message.js
+++ routes/text-message.js
@@ -10,6 +10,8 @@ const utils = require('./../lib/utils');
 router.post('/', function(req, res) {
   console.log('received a text-message request for e-mail');
 
+  console.log(req.body);
+
   const emailAddress = utils.extractEmailFromString(req.body.Body);
 
   if(emailAddress.length > 0 && validator.isEmail(emailAddress)) {
