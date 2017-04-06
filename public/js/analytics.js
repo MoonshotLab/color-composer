a2719c4b37cb11fbe75d2e21b45ca8a0661e1261
Preston Richey <prichey@barkleyus.com>
2017-04-06 13:45:32 -0500

    Update google analytics id

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -3,7 +3,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-92778091-1', 'auto');
+ga('create', 'UA-8476038-37', 'auto');
 ga('send', {
   hitType: 'pageView',
   page: 'tutorial',
