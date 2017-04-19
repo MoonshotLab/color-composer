2c2b89b793ac64abd143b3d96f64fcd06126fed8
Preston Richey <prichey@barkleyus.com>
2017-04-19 17:24:39 -0500

    Move tutorial video url from template to route; Add different video file for desktop

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ const util = require('./../lib/util');
 
 router.get('/', function (req, res) {
   const md = new MobileDetect(req.headers['user-agent']);
+  const desktopVideoUrl = 'https://s3.amazonaws.com/nelson-atkins/cc_desktop_intro_1080.mp4';
+  const galleryVideoUrl = 'https://s3.amazonaws.com/nelson-atkins/cc_intro.mp4';
 
   if (md.mobile() !== null) {
     // it's mobile!
@@ -15,7 +17,14 @@ router.get('/', function (req, res) {
       bodyClasses: 'mobile'
     });
   } else {
-    const template = (process.env.LOCATION === 'gallery' ? 'gallery' : 'desktop');
+    let template, videoUrl;
+    if (process.env.LOCATION === 'gallery') {
+      template = 'gallery';
+      videoUrl = galleryVideoUrl;
+    } else {
+      template = 'desktop';
+      videoUrl = desktopVideoUrl;
+    }
 
     res.render(`pages/${template}`, {
       config: clientConfig,
@@ -24,7 +33,8 @@ router.get('/', function (req, res) {
       location: process.env.LOCATION,
       bodyClasses: `video-playing ${process.env.LOCATION}`,
       bodyId: 'body',
-      title: 'Color Composer'
+      title: 'Color Composer',
+      videoUrl: videoUrl
     });
   }
 });
